2e35617db25869eca9cf2501b8524797908e1934
BaoyuanZhou <112027789+BaoyuanZhou@users.noreply.github.com>
2023-02-05 22:20:41 -0600

    Create part2.sol

---
+++ part2.sol
@@ -0,0 +1,15 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.9;
+
+import "ibcp2.sol";
+
+contract myContract{
+    IBDevTaskPart2 public P2contract;
+    constructor(address _P2address){
+        P2contract=IBDevTaskPart2(_P2address);
+    }
+    function sendTask(string calldata message) public{
+        P2contract.sendTask(message);
+    }
+}
